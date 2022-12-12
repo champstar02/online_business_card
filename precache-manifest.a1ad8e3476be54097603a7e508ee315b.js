ce494c40d62d06d3dd2f71298421c7dff3371f8d
Tharun Balaji <69449535+champstar02@users.noreply.github.com>
2022-12-12 19:04:53 +0530

    Add files via upload

---
+++ precache-manifest.a1ad8e3476be54097603a7e508ee315b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d983b1400c11ab06e1e1",
+    "url": "/static/css/main.c05a75e2.chunk.css"
+  },
+  {
+    "revision": "d983b1400c11ab06e1e1",
+    "url": "/static/js/main.d983b140.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "fa1e30c6e6c64559af32",
+    "url": "/static/js/runtime~main.fa1e30c6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "d5b5382fb3aee47085f0f696d0b59ddf",
+    "url": "/index.html"
+  }
+];
